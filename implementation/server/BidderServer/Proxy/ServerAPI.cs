36ba6c215622f20ffa9391baed1e2fcb9684d593
Jan Lejnar <lejnar@komix.cz>
2019-11-11 14:42:30 -0600

    progress

---
+++ implementation/server/BidderServer/Proxy/ServerAPI.cs
@@ -0,0 +1,15 @@
+﻿using BidderClient.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BidderServer.Proxy
+{
+    public interface ServerAPI
+    {
+        bool autentizate(string userName, string password);
+        bool bidProduct(int productID, double bidValue, User bidder);
+    }
+}
