05e266359ef6a452db4ef257014a707c60ad01c7
Jan Lejnar <lejnar@komix.cz>
2019-11-11 15:36:47 -0600

    Second view

--- implementation/server/BidderServer/Program.cs
+++ implementation/server/BidderServer/Program.cs
@@ -16,7 +16,7 @@ namespace BidderServer
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ManageProductsForm());
+            // Application.Run(new ManageProductsForm());
         }
     }
 }
