591f67cb35754ac2535e0d3eebe526acedd028c6
milanzongor <mzongor29@gmail.com>
2019-11-11 12:24:04 -0600

    ClientModel created

---
+++ implementation/client/BidderClient/ClientModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BidderClient
+{
+    class ClientModel
+    {
+        public List<Shared.Product> productInventory { get; set; }
+        public Shared.User User { get; set; }
+
+    }
+}
---
+++ implementation/client/BidderClient/ClientState.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BidderClient
+{
+    public enum ClientState { UNAUTENTIZED, AUTENTIZED_SUCCESSFULLY, AUTENTIZATION_FAILED, ALL_PRODUCTS_OFFERED, PRODUCT_SELECTED, BID_PLACED_OK, BID_REJECTEDPPED };
+}
