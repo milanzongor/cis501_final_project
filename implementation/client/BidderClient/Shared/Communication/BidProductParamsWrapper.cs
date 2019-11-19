008bd02be53d63d497d16cf0fc031c509100d3da
Jan Lejnar <lejnar@komix.cz>
2019-11-19 16:52:07 -0600

    wrappers created so I will be able to use json easily

---
+++ implementation/client/BidderClient/Shared/Communication/BidProductParamsWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BidderClient.Shared.Communication
+{
+    class BidProductParamsWrapper
+    {
+        public int productID { get; set; }
+        public double bidValue { get; set; }
+        public User bidder { get; set; }
+    }
+}
---
+++ implementation/server/BidderServer/Shared/Communication/UpdateProductsWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BidderClient.Shared.Communication
+{
+    class UpdateProductsWrapper
+    {
+        public List<Product> productsInventory { get; set; }
+    }
+}
