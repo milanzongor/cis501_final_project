40431fe7d87beb2e9102459244067e9a5cca9d21
Jan Lejnar <lejnar@komix.cz>
2019-12-07 12:21:24 -0600

    Fixed problem with message response

--- implementation/client/BidderClient/Proxy/ServerProxy.cs
+++ implementation/client/BidderClient/Proxy/ServerProxy.cs
@@ -12,7 +12,7 @@ using WebSocketSharp.Server;
 
 namespace BidderClient.Proxy
 {
-    public class ServerProxy : WebSocketBehavior, ServerAPI
+    public class ServerProxy : ServerAPI
     {
         private WebSocket webSocketToRealServer;
         private Dictionary<int, Product> productsInventory;
@@ -27,6 +27,34 @@ namespace BidderClient.Proxy
             this.webSocketToRealServer = new WebSocket(REAL_SERVER_URL);
             this.webSocketToRealServer.Log.Level = LogLevel.Debug;
             this.webSocketToRealServer.Connect();
+            this.webSocketToRealServer.OnMessage += (sender, e) =>
+            {
+                Console.WriteLine("Server says: " + e.Data);
+                UpdateProductsParamWrapper updateProductsParam = JsonConvert.DeserializeObject<UpdateProductsParamWrapper>(e.Data);
+                if (updateProductsParam.hasValidValues())
+                {
+                    // update products message came
+                    Console.WriteLine("Update products inventory message came");
+                    this.productsInventory = updateProductsParam.productsInventory;
+                }
+                else
+                {   // information about result of either previous authentization or previous bidding must have come
+                    DidUserAutentizeWrapper didUserAutentize = JsonConvert.DeserializeObject<DidUserAutentizeWrapper>(e.Data);
+                    if (didUserAutentize.hasValidValues())
+                    {
+                        // autentization result message came
+                        Console.WriteLine("Autentization result message came");
+                        this.didUserAutentizeWrapper = didUserAutentize;
+                    }
+                    else
+                    {
+                        // bidding result message came
+                        Console.WriteLine("Bidding result message came");
+                        WasBidPlacedWrapper wasBidPlaced = JsonConvert.DeserializeObject<WasBidPlacedWrapper>(e.Data);
+                        this.wasBidPlacedWrapper = wasBidPlaced;
+                    }
+                }
+            };
         }
 
         ~ServerProxy()
@@ -39,18 +67,17 @@ namespace BidderClient.Proxy
             if (webSocketToRealServer.IsAlive)
             {
                 webSocketToRealServer.Send(JsonConvert.SerializeObject(credentials));
-                //while (didUserAutentizeWrapper == null) // while no response from server came
-                //{
-                //    Thread.Sleep(50); // slow poll wait for response
-                //}
-                //if (didUserAutentizeWrapper.wasSuccessful)
-                //{
-                //    return didUserAutentizeWrapper.autentizedUser;
-                //} else
-                //{
-                //    return null;
-                //}
-                return new User(1, new Credentials("testingUser", "testingPassword"));
+                while (didUserAutentizeWrapper == null) // while no response from server came
+                {
+                    Thread.Sleep(50); // slow poll wait for response
+                }
+                if (didUserAutentizeWrapper.wasSuccessful)
+                {
+                    return didUserAutentizeWrapper.autentizedUser;
+                } else
+                {
+                    return null;
+                }
             }
             else
             {
@@ -83,32 +110,5 @@ namespace BidderClient.Proxy
             return productsInventory;
         }
 
-        protected override void OnMessage(MessageEventArgs e)
-        {
-            Console.WriteLine("Server says: " + e.Data);
-            UpdateProductsParamWrapper updateProductsParam = JsonConvert.DeserializeObject<UpdateProductsParamWrapper>(e.Data);
-            if (updateProductsParam.hasValidValues())
-            {
-                // update products message came
-                Console.WriteLine("Update products inventory message came");
-                this.productsInventory = updateProductsParam.productsInventory;
-            }
-            else
-            {   // information about result of either previous authentization or previous bidding must have come
-                DidUserAutentizeWrapper didUserAutentize = JsonConvert.DeserializeObject<DidUserAutentizeWrapper>(e.Data);
-                if (didUserAutentize.hasValidValues())
-                {
-                    // autentization result message came
-                    Console.WriteLine("Autentization result message came");
-                    this.didUserAutentizeWrapper = didUserAutentize;
-                } else
-                {
-                    // bidding result message came
-                    Console.WriteLine("Bidding result message came");
-                    WasBidPlacedWrapper wasBidPlaced = JsonConvert.DeserializeObject<WasBidPlacedWrapper>(e.Data);
-                    this.wasBidPlacedWrapper = wasBidPlaced;
-                }
-            }
-        }
     }
 }
