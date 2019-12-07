d13c20158459146ecb0ee0332e93d8fca49fffed
milanzongor <mzongor29@gmail.com>
2019-12-07 15:02:11 -0600

    login bug fixed

--- implementation/client/BidderClient/Proxy/ServerProxy.cs
+++ implementation/client/BidderClient/Proxy/ServerProxy.cs
@@ -76,11 +76,13 @@ namespace BidderClient.Proxy
                     return didUserAutentizeWrapper.autentizedUser;
                 } else
                 {
+                    didUserAutentizeWrapper = null;
                     return null;
                 }
             }
             else
             {
+                didUserAutentizeWrapper = null;
                 return null;
             }
         }
@@ -95,7 +97,7 @@ namespace BidderClient.Proxy
                 );
                 while (wasBidPlacedWrapper == null)
                 {
-                    Thread.Sleep(25); // slow poll wait for response
+                    Thread.Sleep(50); // slow poll wait for response
                 }
                 return wasBidPlacedWrapper.wasSuccessful;
             }
