e6f7cb60c2318f9fbcaeb46da85d761e6a5810c7
Jan Lejnar <lejnar@komix.cz>
2019-12-07 16:03:46 -0600

    ProductAuction wrapper fix

---
+++ implementation/client/BidderClient/Shared/Communication/ProductAuctionResultWrapper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BidderClient.Shared.Communication
+{
+    public class ProductAuctionResultWrapper
+    {
+        public Product product { get; set; }
+        public bool didYouWin { get; set; }
+
+        public ProductAuctionResultWrapper()
+        {
+           
+        }
+
+        public ProductAuctionResultWrapper(Product product, bool didYouWin)
+        {
+            this.product = product;
+            this.didYouWin = didYouWin;
+        }
+        public bool hasValidValues()
+        {
+            return product != null && product.item != null;
+        }
+    }
+}
